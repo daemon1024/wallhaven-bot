cd8becc36f6fc88dd5dd22efe9499ec444254a5d
daemon1024 <barun1024@gmail.com>
2020-04-25 18:46:17 +0530

    add nsfw search command

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
-// require("dotenv").config();
+require("dotenv").config();
 const API_URL = "https://wallhaven.cc/api/v1/";
 const search = async (query) => {
   let args = query.split(" ");
   let q = "search?q=" + args.join("+");
   let data = await axios.get(API_URL + q + "&sorting=random");
   let resp = (await data.data.data[0]) ? data.data.data[0].path : "404";
+  return resp;
+};
+const nsfw = async () => {
+  let data = await axios.get(
+    API_URL +
+      "search?sorting=random" +
+      "&purity=001" +
+      "&apikey=" +
+      process.env.WALLHAVEN_API_KEY
+  );
+  let resp = await data.data.data[1].path;
   return resp;
 };
 const random = async () => {
@@ -27,5 +38,10 @@ bot.onText(/\/search (.+)/, async (msg, match) => {
 bot.onText(/\/random/, async (msg) => {
   const chatId = msg.chat.id;
   const resp = await random();
+  bot.sendDocument(chatId, resp);
+});
+bot.onText(/\/nsfw/, async (msg) => {
+  const chatId = msg.chat.id;
+  const resp = await nsfw();
   bot.sendDocument(chatId, resp);
 });
